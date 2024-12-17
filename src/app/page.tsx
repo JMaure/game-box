c953f88a0da441d33c9ba8cda9ffe9b9d12c5ed5
Julien <julienmaure.pro@gmail.com>
2024-12-17 12:31:08 +0100

    add a header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Header } from "./Header";
 import { MenuGames } from "./MenuGames";
 import { useSearchParams } from "next/navigation";
 
@@ -12,15 +13,25 @@ export default function Home() {
   console.log(currentGame);
 
   return (
-    <main className="m-auto h-h-full flex max-w-4xl px-4">
-      <div className="mb-4 mt-8 flex flex-1 gap-4 py-2  max-lg:flex-col">
-        <MenuGames games={games} currentGame={currentGame} />
-      </div>
-      <div className="mb-4 mt-8 mx-4 h-fit w-full">
-        {currentGame === "" && <p>Home Page</p>}
-        {currentGame === "TicTacToe" && <p>Tic Tac Toe</p>}
-        {currentGame === "Minesweeper" && <p>Minesweeper</p>}
-        {currentGame === "Snake" && <p>Snake</p>}
+    <main className="max-w-4xl m-auto border border-solid border-gray-800s">
+      <Header />
+      <div className="m-auto h-full flex">
+        <div className="px-4 py-8 flex flex-1 gap-4 bg-primary/50 max-lg:flex-col">
+          <MenuGames games={games} currentGame={currentGame} />
+        </div>
+        <div className="mb-4 mt-8 mx-4 h-fit w-full">
+          {currentGame === "" && (
+            <p className="text-justify  text-lg">
+              Envie de vous détendre et de vous amuser ? Game Box est fait pour
+              vous ! Redécouvrez les jeux qui ont marqué votre enfance ou
+              initiez-vous à de nouveaux classiques. Avec Game Box, le fun est
+              garanti !
+            </p>
+          )}
+          {currentGame === "TicTacToe" && <p>Tic Tac Toe</p>}
+          {currentGame === "Minesweeper" && <p>Minesweeper</p>}
+          {currentGame === "Snake" && <p>Snake</p>}
+        </div>
       </div>
     </main>
   );
